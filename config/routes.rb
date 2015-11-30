2f1868a598c97b61634d59c5224638098ec4ae65
Ryan King <hello@ryanking.com>
2015-11-29 19:49:45 -0500

    Fix duplicate rendering bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'static_pages#home'
-  get 'about' =>'static_pages#about'
+  get 'about' => 'static_pages#about'
   # get 'signup' => 'users#new'
   # get 'login' => 'sessions#new'
   # post 'login' =>'sessions#create'
