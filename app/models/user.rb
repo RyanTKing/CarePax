8b43699af3fe6cf7247771be1ca1d5159209b66b
Baiming Zhang <Gigamirror@wireless1x-155-41-66-41.bu.edu>
2015-12-02 18:54:37 -0500

    More Models Generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255},
+					  format: { with: VALID_EMAIL_REGEX},
+					  uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6}
+    validates :fname, presence: true, length: { maximum: 35}
+    validates :lname, presence: true, length: { maximum: 35}
+    validates :hometown, length: { maximum: 255}
+    validates :gender, length: { maximum: 6}
+    validates :bio, length: {maximum: 1000}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+      @user = User.new(name: "Test", email: "test@test.com", password: "testtest", password_confirmation: "testtest", fname: "baiming", lname: "zhang")
+  end
+
 end
