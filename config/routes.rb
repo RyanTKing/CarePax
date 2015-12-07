fe12c81fd0a070d391a0bd7389d65f272d335ef7
David Monti <davidmonti@43-13-41-155-wireles1x.bu.edu>
2015-12-07 16:18:53 -0500

    Add Help Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'order' => 'static_pages#order'
+  get 'help' => 'static_pages#help'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
