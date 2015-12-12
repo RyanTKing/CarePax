df907afc6cae0566bff835465fe2c8dc5aeec71b
Ryan King <hello@ryanking.com>
2015-12-12 17:00:44 -0500

    Fix driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,7 @@
 class Driver < ActiveRecord::Base
   belongs_to :user
 
-  validates :plate, presence: true, length: 6
+  validates :plate, presence: true, length: { is: 6 }
   validates :make, presence: true
   validates :model, presence: true
   validates :model, presence: true
